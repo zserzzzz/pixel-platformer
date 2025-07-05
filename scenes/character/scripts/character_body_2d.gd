93192f6d354e373479d095fa82c0a40b88b96308
zserzzzz <gam3r.zaka@gmail.com>
2025-07-05 20:11:34 +0100

    final commit
    maybe

--- scenes/character/scripts/character_body_2d.gd
+++ scenes/character/scripts/character_body_2d.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 @onready var ttm: Timer = $ttm
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var direction
+var directionx
+var directiony
 
 var gravity = Vector2(0,980.0)
 
@@ -19,20 +20,22 @@ func _physics_process(delta: float) -> void:
 		velocity += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("w") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	
 	if ttm.time_left == 0:
 		gravity = Vector2(0,980.0)
+		animated_sprite_2d.play("default")
 		walk_extra()
 	
 	if Input.is_action_just_pressed("dash") and cd.time_left == 0:
 		print("dashed")
-		direction = Input.get_axis("a", "d")
-		velocity.x += direction * DASH_SPEED
+		directionx = Input.get_axis("a", "d")
+		directiony = Input.get_axis("w","s")
+		velocity = Vector2(directionx * DASH_SPEED,directiony * DASH_SPEED * 0.75)
 		dash_extra()
-		if direction == -1:
+		if directionx == -1:
 			animated_sprite_2d.play("dash_left")
 		else:
 			animated_sprite_2d.play("dash_right")
@@ -47,8 +50,9 @@ func dash_extra():
 	gravity = Vector2(0,0)
 	
 func walk_extra():
-	direction = Input.get_axis("a", "d")
-	if direction:
-		velocity.x = direction * SPEED
+	directionx = Input.get_axis("a", "d")
+	
+	if directionx:
+		velocity.x = directionx * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
